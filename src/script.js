0c0972c325bbcdfe3a5597f14388038ec5459ff1
klezi10 <74952593+klezi10@users.noreply.github.com>
2021-09-04 17:23:52 +0700

    Changed description of weather API data pull

--- src/script.js
+++ src/script.js
@@ -45,7 +45,7 @@ function displayWeather(response) {
     response.data.wind.speed
   );
   document.querySelector('#description').innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
 }
 
 function searchCity(city) {
